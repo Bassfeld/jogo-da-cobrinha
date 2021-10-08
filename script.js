5e24ce4818bd67b864f339c161fedbd4e80be773
Bassfeld <jonas.bassfeld@gmail.com>
2021-10-08 20:23:38 -0300

    define final do jogo e estiliza página

--- script.js
+++ script.js
@@ -59,11 +59,20 @@ function update(event){
 }
 
 function iniciarJogo(){
+
     /*define condicional para cobrinha voltar ao início do eixo ao ultrapassar limite do canvas*/
     if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+    /*define condicional para fim de jogo */
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x ==  snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over :( recarregue a página para reiniciar o jogo");
+        }
+    }
 
     criarBG();
     criarCobrinha();
